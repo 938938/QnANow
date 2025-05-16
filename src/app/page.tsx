4ba8f4fd75cf614712869171ce2919608d3df485
938938 <938kjh@gmail.com>
2025-05-16 23:36:03 +0900

    [FEAT] 답변 영역 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Answer from '@/components/Answer';
 import AskInput from '@/components/AskInput';
+import QuestionList from '@/components/QuestionList';
 
 export default function Home() {
   return (
     <div>
       <AskInput />
+      <Answer />
+      <QuestionList />
     </div>
   );
 }
