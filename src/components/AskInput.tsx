45462a594368e38216d8d5e8efc40500bc04d718
938938 <938kjh@gmail.com>
2025-05-17 23:22:40 +0900

    [FEAT] 질문 기본 세팅

--- src/components/AskInput.tsx
+++ src/components/AskInput.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+'use client';
+
+import { useState } from 'react';
 
 const AskInput = () => {
+  const [ask, setAsk] = useState<string>('');
+  const onClickHandler = async () => {
+    console.log(ask);
+    setAsk('');
+  };
   return (
     <div className='flex flex-col mx-auto max-w-screen-sm gap-2 my-2'>
-      <input className='border' />
-      <button className='bg-slate-300'>질문하기</button>
+      <input
+        className='border'
+        value={ask}
+        onChange={(e) => setAsk(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault(); // 엔터 시 기본 제출 방지
+            onClickHandler();
+          }
+        }}
+      />
+      <button className='bg-slate-300' onClick={onClickHandler}>
+        질문하기
+      </button>
     </div>
   );
 };
