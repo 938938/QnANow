7c0ce0dca73c7d29fb7255794263c1c66b044d6b
938938 <938kjh@gmail.com>
2025-05-22 23:49:09 +0900

    [FEAT] 서버 연결

--- src/utils/redux/oxSlice.ts
+++ src/utils/redux/oxSlice.ts
@@ -1,10 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export type OxState = {
   question: string;
   answer: string;
   pic: string;
 };
+
+const fetchAnswer = createAsyncThunk('/question/ox', async (_, thunkAPI) => {
+  try {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_OX_SERVER}`);
+    return res.data;
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err);
+  }
+});
 
 const initialState: OxState = {
   question: '',
@@ -16,8 +26,19 @@ const oxSlice = createSlice({
   name: 'ox',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchAnswer.pending, (state) => {
+        state.answer = '대답을 가져오는 중입니다';
+      })
+      .addCase(fetchAnswer.fulfilled, (state, action) => {
+        console.log(action.payload);
+        state.answer = action.payload;
+      })
+      .addCase(fetchAnswer.rejected, (state) => {
+        state.answer = '대답을 가져오는 것에 실패했습니다.';
+      });
+  },
 });
-
-export const {} = oxSlice.actions;
 
 export default oxSlice.reducer;
