df6a41d2e877d6d0ad270dadd629cd967462821a
938938 <938kjh@gmail.com>
2025-05-23 01:03:42 +0900

    [FEAT] oxSlice 설정

--- src/utils/redux/oxSlice.ts
+++ src/utils/redux/oxSlice.ts
@@ -1,16 +1,22 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export type OxState = {
   question: string;
   answer: string;
   pic: string;
+  loading: boolean;
+  error: string | null;
 };
 
 const fetchAnswer = createAsyncThunk('/question/ox', async (_, thunkAPI) => {
   try {
     const res = await axios.get(`${process.env.NEXT_PUBLIC_OX_SERVER}`);
-    return res.data;
+    console.log(res.data);
+    return {
+      answer: res.data.answer,
+      pic: res.data.image,
+    };
   } catch (err) {
     return thunkAPI.rejectWithValue(err);
   }
@@ -20,25 +26,35 @@ const initialState: OxState = {
   question: '',
   answer: '',
   pic: '',
+  loading: false,
+  error: null,
 };
 
 const oxSlice = createSlice({
   name: 'ox',
   initialState,
-  reducers: {},
+  reducers: {
+    setQuestion(state, action: PayloadAction<string>) {
+      state.question = action.payload;
+    },
+  },
   extraReducers(builder) {
     builder
       .addCase(fetchAnswer.pending, (state) => {
-        state.answer = '대답을 가져오는 중입니다';
+        state.loading = true;
+        state.error = null;
       })
       .addCase(fetchAnswer.fulfilled, (state, action) => {
-        console.log(action.payload);
-        state.answer = action.payload;
+        state.loading = false;
+        state.answer = action.payload.answer;
+        state.pic = action.payload.pic;
       })
-      .addCase(fetchAnswer.rejected, (state) => {
-        state.answer = '대답을 가져오는 것에 실패했습니다.';
+      .addCase(fetchAnswer.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
       });
   },
 });
 
+export const { setQuestion } = oxSlice.actions;
 export default oxSlice.reducer;
