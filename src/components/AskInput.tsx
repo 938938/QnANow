94c46bd1d1115d89101ac6edcf9ed9f12cc122f7
938938 <938kjh@gmail.com>
2025-05-25 23:39:24 +0900

    [FEAT] 공용버튼컴포넌트 생성

--- src/components/AskInput.tsx
+++ src/components/AskInput.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch } from '@/utils/hooks/hooks';
 import { fetchOxAnswer, setOxAsk } from '@/utils/redux/oxSlice';
 import { useState } from 'react';
 import InputComponents from './common/InputComponents';
+import ButtonComponents from './common/ButtonComponents';
 
 const AskInput = () => {
   const [ask, setAsk] = useState<string>('');
@@ -14,15 +15,13 @@ const AskInput = () => {
     setAsk('');
   };
   return (
-    <div className='flex flex-col mx-auto max-w-screen-sm gap-2 my-2'>
+    <div className='flex flex-col mx-auto max-w-screen-sm my-2'>
       <InputComponents
         value={ask}
         setValue={setAsk}
         onClickHandler={onClickHandler}
       />
-      <button className='bg-slate-300' onClick={onClickHandler}>
-        질문하기
-      </button>
+      <ButtonComponents text='질문하기' onClickHandler={onClickHandler} />
     </div>
   );
 };
---
+++ src/components/common/ButtonComponents.tsx
@@ -0,0 +1,18 @@
+const ButtonComponents = ({
+  text,
+  onClickHandler,
+}: {
+  text: string;
+  onClickHandler: () => void;
+}) => {
+  return (
+    <button
+      onClick={onClickHandler}
+      className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none'
+    >
+      {text}
+    </button>
+  );
+};
+
+export default ButtonComponents;
