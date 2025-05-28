36f3dbe2cb02d715262c559b0c2b51a4493f599b
938938 <938kjh@gmail.com>
2025-05-28 21:46:58 +0900

    [FEAT] gemini ai 연결

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+'use client';
+import ButtonComponents from '@/components/common/ButtonComponents';
+import InputComponents from '@/components/common/InputComponents';
+import { GoogleGenAI } from '@google/genai';
+import { useState } from 'react';
+
+const ai = new GoogleGenAI({
+  apiKey: `${process.env.NEXT_PUBLIC_TOKEN}`,
+});
+
+const askAiHandler = async (question: string) => {
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.0-flash',
+    contents: `다음 질문에 대해 친절하고 간단하게 한 문장으로 대답해줘:\n"${question}"`,
+  });
+  return response.text;
+};
 
 const AIpage = () => {
-  return <div>AIpage</div>;
+  const [ask, setAsk] = useState<string>('');
+  const onClickHandler = async () => {
+    if (!ask) return;
+    const answer = await askAiHandler(ask);
+    console.log('AI 응답:', answer);
+  };
+  return (
+    <div>
+      <InputComponents
+        value={ask}
+        setValue={setAsk}
+        onClickHandler={onClickHandler}
+      />
+      <ButtonComponents text='질문하기' onClickHandler={onClickHandler} />
+    </div>
+  );
 };
 
 export default AIpage;
