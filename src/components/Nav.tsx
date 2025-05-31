a50ca108de99a42c20123f9f852a2f605dac0742
938938 <938kjh@gmail.com>
2025-05-31 23:53:10 +0900

    [DESIGN] Nav 스타일 변경

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,26 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Nav = () => {
+  const pathname = usePathname();
   return (
-    <div className='flex gap-2'>
-      <Link href={'/'}>
+    <div className='flex absolute -top-11 gap-1'>
+      <Link
+        href={'/'}
+        className={`bg-defaultWhite border-2 border-solid border-defaultGreen p-2 ${
+          pathname === '/' && 'border-b-0'
+        }`}
+      >
         <button>OX로 답변받기</button>
       </Link>
-      <Link href={'/ai'}>
+      <Link
+        href={'/ai'}
+        className={`bg-defaultWhite border-2 border-solid border-defaultGreen p-2 ${
+          pathname === '/ai' && 'border-b-0'
+        }`}
+      >
         <button>AI에게 물어보기</button>
       </Link>
     </div>
