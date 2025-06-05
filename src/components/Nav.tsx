8873da48e473a457beaed13c75334c6a051a759b
938938 <938kjh@gmail.com>
2025-06-05 23:01:04 +0900

    [REFACTOR] Nav 중복 코드 제거

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,26 +3,27 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
+const navItems = [
+  { href: '/', label: 'OX로 답변받기' },
+  { href: '/ai', label: 'AI에게 물어보기' },
+];
+
 const Nav = () => {
   const pathname = usePathname();
+  const linkStyleHandler = (href: string) => {
+    const base =
+      'bg-defaultWhite border-2 border-solid border-defaultGreen p-2';
+    const active = 'border-b-0 bg-white';
+    return `${base} ${pathname === href ? active : ''}`;
+  };
+
   return (
     <div className='flex absolute -top-11 gap-1'>
-      <Link
-        href={'/'}
-        className={`bg-defaultWhite border-2 border-solid border-defaultGreen p-2 ${
-          pathname === '/' && 'border-b-0'
-        }`}
-      >
-        <button>OX로 답변받기</button>
-      </Link>
-      <Link
-        href={'/ai'}
-        className={`bg-defaultWhite border-2 border-solid border-defaultGreen p-2 ${
-          pathname === '/ai' && 'border-b-0'
-        }`}
-      >
-        <button>AI에게 물어보기</button>
-      </Link>
+      {navItems.map(({ href, label }) => (
+        <Link key={href} href={href} className={linkStyleHandler(href)}>
+          {label}
+        </Link>
+      ))}
     </div>
   );
 };
