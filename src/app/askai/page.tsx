038700cdd6bda87f76dd01ba3703b7a1a12e808a
938938 <938kjh@gmail.com>
2025-06-25 20:47:09 +0900

    [FEAT] 안내문 추가

--- src/app/askai/page.tsx
+++ src/app/askai/page.tsx
@@ -7,8 +7,9 @@ const AIpage = () => {
   return (
     <div className='flex flex-col mx-auto max-w-screen-sm my-2'>
       <Title
-        title='Ask AI!'
+        title='Ask AI !'
         sub='간단한 질문을 해보세요, 정성껏 답변해드릴게요!'
+        warn={`AI 질문과 답변은 모든 사용자에게 공유됩니다.`}
       />
       <AITooltips />
       <AIAskInput />
--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -1,10 +1,21 @@
-const Title = ({ title, sub }: { title: string; sub: string }) => {
+const Title = ({
+  title,
+  sub,
+  warn,
+}: {
+  title: string;
+  sub: string;
+  warn?: string;
+}) => {
   return (
     <div>
       <h1 className='my-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl'>
         {title}
       </h1>
-      <p className='mb-4'>&#62; {sub}</p>
+      <div className='mb-4'>
+        <p>&#62; {sub}</p>
+        {warn && <p className='text-defaultRadicalred'>{warn}</p>}
+      </div>
     </div>
   );
 };
