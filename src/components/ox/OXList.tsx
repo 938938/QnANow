74547b2ba7b93e30529256680414d2f96e058c1c
938938 <938kjh@gmail.com>
2025-07-01 23:50:31 +0900

    [FEAT] OXList 기본설정

--- src/components/ox/OXList.tsx
+++ src/components/ox/OXList.tsx
@@ -1,7 +1,28 @@
-const OXList = () => {
-  return (
-    <div>OXList</div>
-  )
-}
+'use client';
 
-export default OXList
+import { useState } from 'react';
+
+const OXList = () => {
+  const [showList, setShowList] = useState<boolean>(false);
+  return (
+    <div className='relative flex top-2'>
+      <button
+        onClick={() => setShowList(!showList)}
+        className='border p-2 rounded'
+      >
+        &#62; 했던 질문 목록
+      </button>
+      {showList && (
+        <ul className='absolute z-50 left-36 bg-white'>
+          <li className='p-2 border '>질문 / 답</li>
+          <li className='p-2 border '>
+            아주아주아주아주아주아주아주아주아주아주아주아주아주아주아주매우긴질문
+            / 답
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default OXList;
