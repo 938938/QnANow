2354c6cd9c627f3ba5aee5371e8a54510279b241
938938 <938kjh@gmail.com>
2025-07-16 22:55:42 +0900

    [FEAT] 리스트 표현의 로직 변경

--- src/components/ox/OXList.tsx
+++ src/components/ox/OXList.tsx
@@ -2,29 +2,20 @@
 
 import useGetList from '@/utils/hooks/useGetList';
 import { useAppSelector } from '@/utils/hooks/useRedux';
-import { useState } from 'react';
 
 const OXList = () => {
   useGetList();
-  const [showList, setShowList] = useState<boolean>(false);
   const { list } = useAppSelector((state) => state.ox);
   return (
-    <div className='relative flex top-2'>
-      <button
-        onClick={() => setShowList(!showList)}
-        className='border p-2 rounded'
-      >
-        &#62; 했던 질문 목록
-      </button>
-      {showList && (
-        <ul className='absolute z-50 left-36 bg-white'>
-          {list.map((ele, idx) => (
-            <li className='p-2 border' key={idx}>
-              {ele.ask} / {ele.answer}
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className='relative flex top-2 group'>
+      <button className='border p-2 rounded'>&#62; 했던 질문 목록</button>
+      <ul className='absolute z-50 left-36 bg-white opacity-0 group-hover:opacity-100'>
+        {list.map((ele, idx) => (
+          <li className='p-2 border' key={idx}>
+            {ele.ask} / {ele.answer}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
