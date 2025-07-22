d782bf3ecf6eac28d9cf58dd7aa5deb2178ceb0a
938938 <938kjh@gmail.com>
2025-07-22 23:34:00 +0900

    [DESIGN] 색상 변경

--- src/components/ox/OXList.tsx
+++ src/components/ox/OXList.tsx
@@ -11,7 +11,14 @@ const OXList = () => {
       <button className='border p-2 rounded'>&#62; 했던 질문 목록</button>
       <ul className='absolute z-50 left-36 bg-white opacity-0 group-hover:opacity-100'>
         {list.map((ele, idx) => (
-          <li className='p-2 border' key={idx}>
+          <li
+            className={`p-2 border border-x-2 cursor-pointer ${
+              ele.bgSet === 'yes'
+                ? 'border-x-defaultDarkturquoise'
+                : 'border-x-defaultRadicalred'
+            }`}
+            key={idx}
+          >
             {ele.ask} / {ele.answer}
           </li>
         ))}
