685d1f9820d3f447ce004fe30d3e4816c7cd0c7c
938938 <938kjh@gmail.com>
2025-07-27 22:47:27 +0900

    [FEAT] 리스트가 없을 때 버튼 표시하지 않음

--- src/components/ox/OXList.tsx
+++ src/components/ox/OXList.tsx
@@ -8,19 +8,23 @@ const OXList = () => {
   useGetList();
   const { list } = useAppSelector((state) => state.ox);
   return (
-    <div className='relative flex top-2 group w-full'>
-      <button className='border p-2 rounded'>&#62; 했던 질문 목록</button>
-      <ul className='absolute z-50 left-36 bg-white opacity-0 group-hover:opacity-100 w-1/2'>
-        {list.map((ele, idx) => (
-          <Accordion
-            key={idx}
-            ask={ele.ask}
-            answer={ele.answer}
-            bgSet={ele.bgSet}
-            pic={ele.pic}
-          />
-        ))}
-      </ul>
+    <div className='relative flex top-2 group w-full h-12'>
+      {list.length > 0 && (
+        <>
+          <button className='border p-2 rounded'>&#62; 했던 질문 목록</button>
+          <ul className='absolute z-50 left-36 bg-white opacity-0 group-hover:opacity-100 w-1/2'>
+            {list.map((ele, idx) => (
+              <Accordion
+                key={idx}
+                ask={ele.ask}
+                answer={ele.answer}
+                bgSet={ele.bgSet}
+                pic={ele.pic}
+              />
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
