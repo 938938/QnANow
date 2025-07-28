e74c710c4a4b9b3143f0cd44d43812307d08618b
938938 <938kjh@gmail.com>
2025-07-28 17:07:41 +0900

    [FEAT] 질문목록표시 구현로직 변경

--- src/components/ox/OXList.tsx
+++ src/components/ox/OXList.tsx
@@ -3,11 +3,13 @@
 import useGetList from '@/utils/hooks/useGetList';
 import { useAppDispatch, useAppSelector } from '@/utils/hooks/useRedux';
 import { setAsk } from '@/utils/redux/oxSlice';
+import { useState } from 'react';
 
 const OXList = () => {
   useGetList();
   const dispatch = useAppDispatch();
   const { list } = useAppSelector((state) => state.ox);
+  const [open, setOpen] = useState<boolean>(false);
   const onClickLiHandler = (idx: number) => {
     const { ask, answer, pic, bgSet } = list[idx];
     const selected = {
@@ -17,27 +19,37 @@ const OXList = () => {
       bgSet,
     };
     dispatch(setAsk(selected));
+    setOpen(false);
   };
   return (
-    <div className='relative flex top-2 group h-12'>
+    <div className='relative flex top-2 h-12'>
       {list.length > 0 && (
         <>
-          <button className='border p-2 rounded'>&#62; 했던 질문 목록</button>
-          <ul className='absolute z-50 left-36 bg-white opacity-0 group-hover:opacity-100'>
-            {list.map((ele, idx) => (
-              <li
-                className={`p-2 border border-x-2 cursor-pointer ${
-                  ele.bgSet === 'yes'
-                    ? 'border-x-defaultDarkturquoise'
-                    : 'border-x-defaultRadicalred'
-                }`}
-                key={idx}
-                onClick={() => onClickLiHandler(idx)}
-              >
-                {ele.ask}
-              </li>
-            ))}
-          </ul>
+          <button
+            className='border p-2 rounded'
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            &#62; 했던 질문 목록
+          </button>
+          {open && (
+            <ul className='absolute z-50 left-36 bg-white'>
+              {list.map((ele, idx) => (
+                <li
+                  className={`p-2 border border-x-2 cursor-pointer ${
+                    ele.bgSet === 'yes'
+                      ? 'border-x-defaultDarkturquoise'
+                      : 'border-x-defaultRadicalred'
+                  }`}
+                  key={idx}
+                  onClick={() => onClickLiHandler(idx)}
+                >
+                  {ele.ask}
+                </li>
+              ))}
+            </ul>
+          )}
         </>
       )}
     </div>
