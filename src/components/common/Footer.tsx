7cbb710210d7520ea1eee9dc86b598bd261cc618
938938 <938kjh@gmail.com>
2025-08-11 17:35:06 +0900

    [DESIGN] 여백 조절

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <p className='text-gray-700 absolute w-full text-center bottom-2'>
+    <p className='text-gray-700 absolute w-full text-center bottom-1'>
       문의·제안: moss.kim.owo@gmail.com
     </p>
   );
--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -9,7 +9,7 @@ const Title = ({
 }) => {
   return (
     <div>
-      <h1 className='my-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl'>
+      <h1 className='my-3 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl'>
         {title}
       </h1>
       <div className='mb-4'>
